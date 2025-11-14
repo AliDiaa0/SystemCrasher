3c88eb0b2b64623b53445ff7459080f7b67eebbb
Ali Diaa <85357930+AliDiaa0@users.noreply.github.com>
2025-11-14 11:15:38 +0300

    Restore original comments and code structure

--- SystemCrasher/Main.c
+++ SystemCrasher/Main.c
@@ -1,4 +1,4 @@
-﻿/* SystemCrasher works from Windows XP to Windows 11, all versions, either 32-bit or 64-bit.For some reason, it does not work on Windows XP 64-bit.
+﻿/* SystemCrasher works from Windows XP to Windows 11, all versions, either 32-bit or 64-bit. For some reason, it does not work on Windows XP 64-bit.
  This does not harm your files or system files, but it can affect the system if you run this for a lot of times, maybe. Do not run it on your real machine if you do not want to cause any damage.
  
 <By Ali Diaa>*/
@@ -64,5 +64,6 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	return 0;
 
 }
+
 
 
